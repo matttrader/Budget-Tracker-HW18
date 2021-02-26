eaadd8fe69e2a0c34736ced70b92464f2bc03f5d
Matt Trader <matthewtrader@sbcglobal.net>
2021-02-26 15:19:27 -0500

    update files

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //error: mongoDB not connecting
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
